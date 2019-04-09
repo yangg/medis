d378aa15c23b59936520037d8f04d9bf431ded3a
Brook Yang <uedsky@gmail.com>
2019-04-09 14:58:45 +0800

    feat: add retryStrategy

--- src/renderer/redux/actions/connection.js
+++ src/renderer/redux/actions/connection.js
@@ -77,8 +77,23 @@ export const connectToRedis = createAction('CONNECT', config => ({getState, disp
       if (config.tlscert) config.tls.cert = config.tlscert;
     }
     const redis = new Redis(Object.assign({}, config, override, {
-      retryStrategy() {
-        return false;
+      retryStrategy(options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with
+            // a individual error
+            return new Error('The server refused the connection');
+        }
+        if (options.total_retry_time > 1000 * 60 * 10) {
+            // End reconnecting after a specific timeout and flush all commands
+            // with a individual error
+            return new Error('Retry time exhausted');
+        }
+        if (options.attempt > 10) {
+            // End reconnecting with built in error
+            return undefined;
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000);
       }
     }));
     redis.defineCommand('setKeepTTL', {
